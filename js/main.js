cc06319b947c971926494111fc86beeeba7afdbd
liza <liza.haritonova@mail.ru>
2023-02-02 17:34:38 +0700

    ferst commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// slider
+$(document).ready(function(){
+	$('.multiple-items').slick({
+		infiniti: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots:true,
+		autoplay: false,
+		autoplaySpeed: 300,
+		centerMode: true,
+		centerPadding: '0',
+		arrows:false,
+		adaptiveHeight: true,
+		variableWidth: true,
+		responsive: [
+			{
+			  breakpoint: 775,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '80px',
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '25px',
+				slidesToShow: 1
+			  }
+			}
+		  ]
+	});
+});
+
+$('.doctors__wrapper-btn_link').on('click', function(){
+	$(this).toggleClass('doctors__wrapper-btn_link--active');
+	$(this).next().slideToggle('200');
+});
